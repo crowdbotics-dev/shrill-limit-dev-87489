87d3dbf9d9356902c5fd42e3f79441fffb225f5b
Crowdbotics <engineering@crowdbotics.com>
2023-05-31 10:26:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shrill_limit_dev_87489/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shrill_limit_dev_87489;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shrill_limit_dev_87489";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-limit-dev-87489",
+  url: "https://shrill-limit-dev-87489.botics.co",
+  api: "https://shrill-limit-dev-87489.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
